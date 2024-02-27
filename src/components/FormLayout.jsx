ceeba0a3df510460dad2f0ed87efeba075318874
pankajkhuswaha <khuswahapankaj00@gmail.com>
2024-02-27 18:06:05 +0530

    modify ; form layout changes

--- src/components/FormLayout.jsx
+++ src/components/FormLayout.jsx
@@ -1,3 +1,4 @@
+import { ArrowLeft, ArrowRight, Save } from "lucide-react";
 import { Navigate } from "react-router";
 import { Link } from "react-router-dom";
 
@@ -17,23 +18,30 @@ const FormLayout = ({
       {children}
 
       {length !== undefined && (
-        <div className="flex justify-between mt-4">
-          <Link to={prev} className="btn lg gray w-1/3 text-center">
+        <div className="flex justify-between mt-4 items-center">
+          <Link
+            to={prev}
+            className="btn gray text-center flex gap-2 text-gray-700 font-semibold"
+          >
+            <ArrowLeft />
             Back
           </Link>
           {!open && (
-            <Link to={next} className="btn lg w-1/3 text-center">
-              Continue
+            <Link
+              to={next}
+              className="btn flex gap-2 text-gray-700 font-semibold"
+            >
+              Next <ArrowRight />
             </Link>
           )}
           {open && (
             <button
-              className="btn lg w-1/3"
+              className="btn flex gap-2 text-gray-700 font-semibold"
               onClick={() => {
                 onSubmit && onSubmit();
               }}
             >
-              Save
+              <Save /> Save
             </button>
           )}
         </div>
